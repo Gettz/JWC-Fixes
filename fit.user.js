da2ab019fa5a61e93b776610c0c87d64a4f8d792
Gettz <49850415+Gettz@users.noreply.github.com>
2019-09-16 14:22:09 -0500

    Update fit.user.js

--- fit.user.js
+++ fit.user.js
@@ -12,11 +12,33 @@
 
 (function() {
 'use strict';
+    var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    try {
+        var frame = document.getElementById('chat');
+        frame.style.height = '80%';
+    }
+    catch(err) {
+    }
+    });
+
+    // define what element should be observed by the observer
+    // and what types of mutations trigger the callback
+    observer.observe(document, {
+        subtree: true,
+        attributes: true,
+        childList: true,
+        characterData: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+    });
+
     //var frame = document.getElementById('groupchatIChat');
     //frame.style.height = '94%';
-    var frame2 = document.getElementsByClassName('rosterInnerElement')[1];
-    frame2.style.height = '75%'
-    //var frame = document.getElementById('chat');
-    //frame.style.height = '80%';
-}
-)();
+    try {
+        var frame2 = document.getElementsByClassName('rosterInnerElement')[1];
+        frame2.style.height = '75%'
+    }
+    catch(err) {
+    }
+})();
